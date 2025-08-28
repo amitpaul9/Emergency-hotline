17aab607d4baa5e4e2cf0e87f7b8b9afe6070e9d
amitpaul9 <atamit905@gmail.com>
2025-08-28 16:48:50 +0600

    Time feauture in call History section added

--- JS/main.js
+++ JS/main.js
@@ -41,16 +41,26 @@ for(const callBtn of callBtns){
         const callHistory = document.getElementById('call-history');
         const newCallHistory = document.createElement('div');
         
+
         // Time feature
-        
+        let now = new Date();
+
+        let hours = now.getHours();
+        let minutes = now.getMinutes();
+        let seconds = now.getSeconds();
+        let ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+
+
+
 
        newCallHistory.innerHTML = `
          <div class="flex justify-between p-4 bg-[#FAFAFA] rounded-[8px] mx-4 mb-2">
     <div >
-        <h1 class="text-[18px] font-semibold ">${serviceTitle}</h1>
+        <h1 class="text-[15px] font-semibold ">${serviceTitle}</h1>
         <p class="text-[#5C5C5C] text-[14px]">${serviceNumber}</p>
     </div>
-    <p>Timne</p>
+    <p>${`${hours}:${minutes}:${seconds} ${ampm}`}</p>
     </div>
         `
         callHistory.append(newCallHistory)
